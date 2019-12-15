f5a6508d1a4597c418c18ed3f61d2c7d525b9131
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-16 00:03:09 +0300

    add Caessor class

---
+++ src/main/java/Caesor.java
@@ -0,0 +1,33 @@
+public class Caesor {
+    private int key;
+
+    /*
+    private final char[] alpha = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
+                                  'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
+                                  'U', 'V', 'W', 'X', 'Y', 'Z'};
+    */
+
+
+    public Caesor(int key) {
+        this.key = key;
+    }
+    public String encrypt(String plainText) {
+        String enc = "";
+        for (int i = 0; i < plainText.length(); i++){
+            char ch = (char)(plainText.codePointAt(i) + key);
+            //char ch = (char)((plainText.codePointAt(i) + key) % 26);
+            enc += ch;
+        }
+        return enc;
+    }
+    public String decrypt(String cipherText) {
+        String dec = "";
+        for (int i = 0; i < cipherText.length(); i++){
+            char ch = (char)(cipherText.codePointAt(i) - key);
+            //char ch = (char)((cipherText.codePointAt(i) - key) % 26);
+            dec += ch;
+        }
+        return dec;
+    }
+}
+
