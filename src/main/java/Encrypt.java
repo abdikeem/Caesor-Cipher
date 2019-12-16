d308bd45076af0892439a1b4c0bc975c9115a875
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-16 09:47:08 +0300

    function change Integer to Array

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -29,4 +29,18 @@ public class Encrypt {
     public boolean isKeyRangeCorrect(int i) {
         return i >= 0 && i <= 25;
     }
+     static String encryptMessageWithKey(String text, int key) {
+        Character[] lc = {'a', 'b', 'c', 'd', 'e', 'f', 'g',
+                'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+                'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
+        List<Integer> integerList = new ArrayList<>();
+        StringBuilder ss = new StringBuilder();
+        List<Character> ciphers = new ArrayList<>();
+        for (int i = 0; i < text.toLowerCase().length(); i++) {
+            for (int i1 = 0; i1 < lc.length; i1++) {
+                if (text.charAt(i) == lc[i1]) {
+                    Integer shift = (i1 + key) % 26;
+                    integerList.add(shift);
+                }
+            }
 }
