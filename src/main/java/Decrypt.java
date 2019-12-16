a78b7879e151af987c962131a2e625875abe4493
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-16 09:59:18 +0300

    add class Decrypt

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,2 +1,19 @@
 public class Decrypt {
+        private String cipherText;
+    private int key;
+
+    public void setCipher(String cipherText) {
+        this.cipherText = cipherText;
+    }
+    public String getCipher() {
+        return this.cipherText;
+    }
+
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+    public int getKey() {
+        return this.key;
+    }
 }
