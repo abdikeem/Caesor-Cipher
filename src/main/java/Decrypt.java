8738050ec15b1b5ea2217e70a06c63115c8550fc
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-16 10:08:28 +0300

    Decrypt content

--- src/main/java/Decrypt.java
+++ src/main/java/Decrypt.java
@@ -1,5 +1,5 @@
 public class Decrypt {
-        private String cipherText;
+    private String cipherText;
     private int key;
 
     public void setCipher(String cipherText) {
@@ -16,10 +16,36 @@ public class Decrypt {
     public int getKey() {
         return this.key;
     }
-     public boolean isCipherString(String testString) {
+
+
+    public boolean isCipherString(String testString) {
         return testString.toLowerCase().matches("[a-z]+");
     }
     public boolean isKeyValid(int key) throws ClassNotFoundException {
         return Class.forName("java.lang.Integer").isInstance(key);
     }
+
+    public boolean isKeyRangeCorrect(int key) {
+        return key >= 0 && key <= 25;
+    }
+
+    public static String decryptMessageWithKey(String cipher, int key) {
+        char[] cipherChars = cipher.toCharArray();
+        char ch;
+        String text = "";
+        for (int i = 0; i < cipher.length(); i++) {
+            if (Character.isLetter(cipherChars[i])) {
+                if (((int) cipherChars[i] - key < 97)) {
+                    ch = (char) (((int) cipherChars[i] - key - 97 + 26) % 26 + 97);
+                } else {
+                    ch = (char) (((int) cipherChars[i] - key - 97) % 26 + 97);
+                }
+                text = text + ch;
+            } else if (cipherChars[i] == ' ') {
+                text = text + cipherChars[i];
+            }
+        }
+        return text;
+    }
+
 }
