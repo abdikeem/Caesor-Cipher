9f2b803ca414cd53f633c3562add8acce9afaf86
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-16 09:38:53 +0300

    add class encrypt

--- src/main/java/Encrypt.java
+++ src/main/java/Encrypt.java
@@ -1,2 +1,22 @@
 public class Encrypt {
+      //init required variables
+    private String text;
+    private Integer key;
+
+    //getters and setters
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getText() {
+        return this.text;
+    }
+
+    public void setKey(int key) {
+        this.key = key;
+    }
+
+    public int getKey() {
+        return this.key;
+    }
 }
--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,7 +1,7 @@
 import org.junit.*;
 import static org.junit.Assert.*;
 
-public class AppTest {
+public class EncryptTest {
 
   @Test
   public void runApp_countUpToOne_ArrayList() {
