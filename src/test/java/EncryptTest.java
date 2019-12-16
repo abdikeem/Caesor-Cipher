ec4ae1d523d9aa722bc3df31956c6eaf29e6ee81
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-16 09:57:08 +0300

    add EncrytTest content

--- src/test/java/EncryptTest.java
+++ src/test/java/EncryptTest.java
@@ -1,10 +1,71 @@
-import org.junit.*;
-import static org.junit.Assert.*;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 public class EncryptTest {
 
-  @Test
-  public void runApp_countUpToOne_ArrayList() {
-    // contents of test will eventually go here.
-  }
+    //Test text getter
+    @Test
+    public void getText_givesText_Returns_Text() {
+        Encrypt encrypt = new Encrypt();
+        encrypt.setText("text");
+        String expected = "text";
+        assertEquals(expected, encrypt.getText());
+    }
+
+    //Test that the text provided is a String
+    @Test
+    public void isTextValid_textIsValid_True() {
+        Encrypt encrypt = new Encrypt();
+        encrypt.setText("text");
+        String testString = encrypt.getText();
+        assertTrue(encrypt.isTextString(testString));
+    }
+
+    //Test Key getter
+    @Test
+    public void getKey_givesKey_Returns_Key() {
+        Encrypt encrypt = new Encrypt();
+        encrypt.setKey(1);
+        int testKey = 1;
+        assertEquals(testKey, encrypt.getKey());
+    }
+
+    //Test key is a number
+    @Test
+    public void isKeyValid_textIsIsValid_True() throws ClassNotFoundException {
+        Encrypt encrypt = new Encrypt();
+        encrypt.setKey(1);
+        assertEquals(true, encrypt.isKeyValid(1));
+    }
+
+    //Test key is between 0 to 25
+    @Test
+    public void isKeyRangeCorrect_returnsCorrect_True() {
+        Encrypt encrypt = new Encrypt();
+        encrypt.setKey(1);
+        assertTrue(encrypt.isKeyRangeCorrect(1));
+    }
+
+    //Test base case of caesar cipher
+    @Test
+    public void encryptMessageWithKey_charA_WithKey1_returnsB() {
+        Encrypt encrypt = new Encrypt();
+        String expected = "b";
+        encrypt.setKey(1);
+        encrypt.setText("a");
+        assertEquals(expected, Encrypt.encryptMessageWithKey(encrypt.getText(), encrypt.getKey()));
+    }
+
+    //Test a random case
+    @Test
+    public void encryptMessageWithKey_StringHello_WithKey10_returns(){
+        Encrypt encrypt = new Encrypt();
+        String expected = "govmywo";
+        encrypt.setKey(10);
+        encrypt.setText("welcome");
+        assertEquals(expected, Encrypt.encryptMessageWithKey(encrypt.getText(), encrypt.getKey()));
+    }
+
 }
