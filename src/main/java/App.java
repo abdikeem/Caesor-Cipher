3fb9279aa58318463d81c54c913687c15f5ed166
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-16 08:47:03 +0300

    add app.java content

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -1,33 +1,7 @@
-public class App{
-    private int key;
-
-    /*
-    private final char[] alpha = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',
-                                  'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T',
-                                  'U', 'V', 'W', 'X', 'Y', 'Z'};
-    */
-
-
-    public App(int key) {
-        this.key = key;
-    }
-    public String encrypt(String plainText) {
-        String enc = "";
-        for (int i = 0; i < plainText.length(); i++){
-            char ch = (char)(plainText.codePointAt(i) + key);
-            //char ch = (char)((plainText.codePointAt(i) + key) % 26);
-            enc += ch;
-        }
-        return enc;
-    }
-    public String decrypt(String cipherText) {
-        String dec = "";
-        for (int i = 0; i < cipherText.length(); i++){
-            char ch = (char)(cipherText.codePointAt(i) - key);
-            //char ch = (char)((cipherText.codePointAt(i) - key) % 26);
-            dec += ch;
-        }
-        return dec;
-    }
+public class App {
+   public static void main (String[] args){
+       system .out.print("1. Encrypt\n2.Decript\nChoose(1,2):");
+       Scanner in = new Scanner(System.in);
+       int choice =in.nextInt();
+   }
 }
-
