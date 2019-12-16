6e3328bb31a4f007e479d14724ac8c60c35a8674
abdikeem <abdihakimabdirahman21@gmail.com>
2019-12-16 10:56:23 +0300

    add setkey

--- src/test/java/DecryptTest.java
+++ src/test/java/DecryptTest.java
@@ -9,5 +9,25 @@ public class DecryptTest {
        String expeted ="kimza";
        assertEquals(expected,decrypt.getCipher());
    }
+@Test
+    public void isCipherTextValid_textIsValid_True() {
+        Decrypt decrypt = new Decrypt();
+        decrypt.setCipher("kimza");
+        String testString = decrypt.getCipher();
+        assertTrue(decrypt.isCipherString(testString));
+    }
+    @Test
+    public void getKey_givesKey_Returns_Key() {
+        Decrypt decrypt = new Decrypt();
+        decrypt.setKey(1);
+        int testKey = 1;
+        assertEquals(testKey, decrypt.getKey());
+    }
+     @Test
+    public void isKeyRangeCorrect_returnsCorrect_True() {
+        Decrypt decrypt = new Decrypt();
+        decrypt.setKey(1);
+        assertTrue(decrypt.isKeyRangeCorrect(1));
+    }
 
 }
